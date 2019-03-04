cd1e3edd80d7df5df316ff64f351592dc7f8293e
Angeline Mulet-Marquis <angelinemm@gmail.com>
2019-03-04 10:14:51 +0000

    If filtering on arcs, show it in mobi title

--- make_ebook.sh
+++ make_ebook.sh
@@ -2,10 +2,22 @@
 
 
 function scrape {
-	echo "Scraping " $1 
-	ruby serial_scrape.rb -s $1 -b $2 -e $3 > ${1}.html
-	ebook-convert ${1}.html ${1}.mobi --authors "John McCrae" --title "${1}" --max-toc-links 500
-	rm ${1}.html
+    TITLE=$1
+    BEGIN=$2
+    END=$3
+    if [ "$BEGIN" -eq 0 ]; then BEGINSTR="start"; else BEGINSTR="arc_$BEGIN"; fi
+    if [ "$END" -eq 999999 ]; then ENDSTR="end"; else ENDSTR="arc_$END"; fi
+    if [ "$BEGIN" -eq 0 ] && [ "$END" -eq 999999 ]; then
+        FULL_TITLE=$TITLE
+    elif [ "$BEGIN" -eq "$END" ]; then
+        FULL_TITLE="${TITLE}_${BEGINSTR}"
+    else
+        FULL_TITLE="${TITLE}_${BEGINSTR}_to_${ENDSTR}"
+    fi
+	echo "Scraping " $TITLE
+	ruby serial_scrape.rb -s $TITLE -b $BEGIN -e $END > ${FULL_TITLE}.html
+	ebook-convert ${FULL_TITLE}.html ${FULL_TITLE}.mobi --authors "John McCrae" --title "${TITLE}" --max-toc-links 500
+	rm ${FULL_TITLE}.html
 }
 BEGIN=0
 END=999999
